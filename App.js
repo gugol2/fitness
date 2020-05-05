ae653ff7d094137392e6565bbc514a9e66e221b8
jsc <jsanchezcriado@gmail.com>
2020-05-05 22:38:39 +0200

    commented out unused code from App

--- App.js
+++ App.js
@@ -8,57 +8,57 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
-import { AddEntry } from './components/AddEntry';
+import AddEntry from './components/AddEntry';
 
 const Stack = createStackNavigator();
 
 export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
+  // const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  // const [initialNavigationState, setInitialNavigationState] = React.useState();
+  // const containerRef = React.useRef();
+  // const { getInitialState } = useLinking(containerRef);
 
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
+  // // Load any resources or data that we need prior to rendering the app
+  // React.useEffect(() => {
+  //   async function loadResourcesAndDataAsync() {
+  //     try {
+  //       SplashScreen.preventAutoHide();
 
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
+  //       // Load our initial navigation state
+  //       setInitialNavigationState(await getInitialState());
 
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
-    }
+  //       // Load fonts
+  //       await Font.loadAsync({
+  //         ...Ionicons.font,
+  //         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+  //       });
+  //     } catch (e) {
+  //       // We might want to provide this error information to an error reporting service
+  //       console.warn(e);
+  //     } finally {
+  //       setLoadingComplete(true);
+  //       SplashScreen.hide();
+  //     }
+  //   }
 
-    loadResourcesAndDataAsync();
-  }, []);
+  //   loadResourcesAndDataAsync();
+  // }, []);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      // <View style={styles.container}>
-      //  {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-      //  <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-      //    <Stack.Navigator>
-      //      <Stack.Screen name="Root" component={BottomTabNavigator} />
-      //    </Stack.Navigator>
-      //  </NavigationContainer>
-      // </View>
-      <View><AddEntry /></View>
-    );
-  }
+  // if (!isLoadingComplete && !props.skipLoadingScreen) {
+  //   return null;
+  // } else {
+  //   return (
+  //     <View>
+  //       <AddEntry />
+  //     </View>
+  //   );
+  // }
+
+  return (
+    <View>
+      <AddEntry />
+    </View>
+  );
 }
 
 // const styles = StyleSheet.create({
