65a52831cfb45f658f0645a4055a606c1dc80496
jsc <jsanchezcriado@gmail.com>
2020-05-05 19:21:34 +0200

    added AddEntry component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from "../utils/helpers";
+import React from 'react';
+import { View } from 'react-native';
+import { getMetricMetaInfo } from '../utils/helpers';
 
-export const AddEntry = (props) => {
-    return <View>{getMetricMetaInfo("bike").getIcon()}</View>;
-}
+export const AddEntry = props => {
+  return <View>{getMetricMetaInfo('bike').getIcon()}</View>;
+};
