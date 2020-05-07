90825cee9d2528a9e3995f7b6ede7603947604df
jsc <jsanchezcriado@gmail.com>
2020-05-07 18:17:03 +0200

    installed redux and created the store

--- App.js
+++ App.js
@@ -1,69 +1,18 @@
 import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { SplashScreen } from 'expo';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import useLinking from './navigation/useLinking';
+import { View } from 'react-native';
 import { AddEntry } from './components/AddEntry';
-
-const Stack = createStackNavigator();
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { entries } from './reducers';
 
 export default function App(props) {
-  // const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  // const [initialNavigationState, setInitialNavigationState] = React.useState();
-  // const containerRef = React.useRef();
-  // const { getInitialState } = useLinking(containerRef);
-
-  // // Load any resources or data that we need prior to rendering the app
-  // React.useEffect(() => {
-  //   async function loadResourcesAndDataAsync() {
-  //     try {
-  //       SplashScreen.preventAutoHide();
-
-  //       // Load our initial navigation state
-  //       setInitialNavigationState(await getInitialState());
-
-  //       // Load fonts
-  //       await Font.loadAsync({
-  //         ...Ionicons.font,
-  //         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-  //       });
-  //     } catch (e) {
-  //       // We might want to provide this error information to an error reporting service
-  //       console.warn(e);
-  //     } finally {
-  //       setLoadingComplete(true);
-  //       SplashScreen.hide();
-  //     }
-  //   }
-
-  //   loadResourcesAndDataAsync();
-  // }, []);
-
-  // if (!isLoadingComplete && !props.skipLoadingScreen) {
-  //   return null;
-  // } else {
-  //   return (
-  //     <View>
-  //       <AddEntry />
-  //     </View>
-  //   );
-  // }
+  const store = createStore(entries);
 
   return (
-    <View>
-      <AddEntry />
-    </View>
+    <Provider store={store}>
+      <View>
+        <AddEntry />
+      </View>
+    </Provider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//   },
-// });
