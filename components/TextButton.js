8efe06d49195631bfab63f6b0ffe438c551ded5d
jsc <jsanchezcriado@gmail.com>
2020-05-10 19:17:29 +0200

    applied styles to TextButton

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { purple } from '../utils/colors';
 
-export const TextButton = ({ children, onPress }) => {
+export const TextButton = ({ children, onPress, style = {} }) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <Text>{children}</Text>
+      <Text style={[styles.reset, style]}>{children}</Text>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: 'center',
+    color: purple,
+  },
+});
