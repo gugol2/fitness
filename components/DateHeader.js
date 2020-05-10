1fb663ff81271b2980caecb768bfd7ffa36985e7
jsc <jsanchezcriado@gmail.com>
2020-05-10 19:25:37 +0200

    syled DateHeader

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { purple } from '../utils/colors';
 
 export const DateHeader = ({ date }) => {
-  return <Text>{date}</Text>;
+  return <Text style={{ color: purple, fontSize: 25 }}>{date}</Text>;
 };
