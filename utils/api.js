db05990652ed2823b39445d21829030f2c770cc9
jsc <jsanchezcriado@gmail.com>
2020-05-10 21:12:42 +0200

    remove the storage first to have clear entries at the beginning

--- utils/api.js
+++ utils/api.js
@@ -2,6 +2,8 @@ import { AsyncStorage } from 'react-native';
 import { CALENDAR_STORAGE_KEY, formatCalendarResults } from './_calendar';
 
 export function fetchCalendarResults() {
+  // Let's clear the storage first
+  AsyncStorage.removeItem(CALENDAR_STORAGE_KEY);
   return AsyncStorage.getItem(CALENDAR_STORAGE_KEY).then(formatCalendarResults);
 }
 
