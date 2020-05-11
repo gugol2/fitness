c7bc736cab788d6816f99b35d97c59bdb0e08051
jsc <jsanchezcriado@gmail.com>
2020-05-11 13:43:19 +0200

    styled renderItem and renderEmptyDate

--- components/History.js
+++ components/History.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { receiveEntries, addEntry } from '../actions';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
 import { fetchCalendarResults } from '../utils/api';
 import UdaciFitnessCalendar from 'udacifitness-calendar-fix';
+import { white } from '../utils/colors';
+import { DateHeader } from './DateHeader';
 
 const History = props => {
   const [ready, setReady] = useState(false);
@@ -28,19 +30,27 @@ const History = props => {
   }, []);
 
   const renderItem = ({ today, ...metrics }, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {today ? (
-        <Text>{JSON.stringify(today)}</Text>
+        <View>
+          <DateHeader date={formattedDate} />
+          <Text style={styles.noDataText}>{today}</Text>
+        </View>
       ) : (
-        <Text>{JSON.stringify(metrics)}</Text>
+        <TouchableOpacity onPress={() => console.log('Pressed!')}>
+          <Text>{JSON.stringify(metrics)}</Text>
+        </TouchableOpacity>
       )}
     </View>
   );
 
   const renderEmptyDate = formattedDate => {
     return (
-      <View>
-        <Text>No Data for this day</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate} />
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
       </View>
     );
   };
@@ -63,3 +73,27 @@ function mapStateToProps(state) {
 }
 
 export const ConnectedHistory = connect(mapStateToProps)(History);
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+});
