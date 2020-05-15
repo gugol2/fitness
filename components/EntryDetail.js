b0f1530dee78fa22c66944093f1132bb4f1be2b2
jsc <jsanchezcriado@gmail.com>
2020-05-15 20:17:01 +0200

    setOptions of navigator in the useLayoutEfect hook

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { Text, View } from 'react-native';
 
 export const EntryDetail = ({ navigation, route }) => {
   const { entryId } = route.params;
+
+  const setTitle = entryId => {
+    if (!entryId) return;
+
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+
+    navigation.setOptions({
+      title: `${month}/${day}/${year}`,
+    });
+  };
+
+  useLayoutEffect(() => {
+    setTitle(entryId);
+  }, [navigation, entryId]);
 
   return (
     <View>
