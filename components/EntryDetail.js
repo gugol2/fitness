8873d9d4233cdbbf6b0261fe362e281c4e6fdaf4
jsc <jsanchezcriado@gmail.com>
2020-05-15 21:22:17 +0200

    use the formattedDate in the Stack Screen for EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,29 +1,31 @@
 import React, { useLayoutEffect, useEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { MetricCard } from './MetricCard';
 import { connect } from 'react-redux';
 import { white } from '../utils/colors';
 
 const EntryDetail = ({ navigation, entryId, metrics, formattedDate }) => {
-  const setTitle = entryId => {
-    if (!entryId) return;
+  // const setTitle = entryId => {
+  //   if (!entryId) return;
 
-    const year = entryId.slice(0, 4);
-    const month = entryId.slice(5, 7);
-    const day = entryId.slice(8);
+  //   const year = entryId.slice(0, 4);
+  //   const month = entryId.slice(5, 7);
+  //   const day = entryId.slice(8);
 
-    navigation.setOptions({
-      title: `${month}/${day}/${year}`,
-    });
-  };
+  //   navigation.setOptions({
+  //     title: `${month}/${day}/${year}`,
+  //   });
+  // };
 
   useLayoutEffect(() => {
-    setTitle(entryId);
+    navigation.setOptions({
+      title: formattedDate,
+    });
   }, [navigation, entryId]);
 
   return (
     <View style={styles.container}>
-      <MetricCard metrics={metrics} date={formattedDate} />
+      <MetricCard metrics={metrics} />
     </View>
   );
 };
