800ce109e898ecba6b9bf147835e658f043b1707
jsc <jsanchezcriado@gmail.com>
2020-05-17 18:24:22 +0200

    When the user does not grat permission nor deny it

--- components/Live.js
+++ components/Live.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import { purple, white } from '../utils/colors';
+import { Foundation } from '@expo/vector-icons';
 
 export const Live = () => {
   const [state, setstate] = useState({
     coords: null,
-    status: null,
+    status: 'undetermined',
     direction: '',
   });
 
   const { status, coords, direction } = state;
+
+  const askPermission = () => {
+    alert('You requested permission!!');
+  };
 
   if (status === null) {
     return <ActivityIndicator style={{ marginTop: 30 }} />;
@@ -24,16 +36,45 @@ export const Live = () => {
 
   if (status === 'undetermined') {
     return (
-      <View>
-        <Text>undetermined</Text>
+      <View style={styles.center}>
+        <Foundation name='alert' size={50} />
+        <Text>You need to enable location services for this app.</Text>
+        <TouchableOpacity style={styles.button} onPress={askPermission}>
+          <Text style={styles.buttonText}>Enable</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Live</Text>
       <Text>{JSON.stringify(state)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  },
+});
